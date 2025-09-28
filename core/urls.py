282f07529f5620f46ec20252b6eb82ce2300dc9c
XXmmRR <Xorex50@protonmial.com>
2025-09-28 23:45:43 +0300

    feat: create user registation endpoint

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('accounts.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ] 
