4fd699a51cbfae3de129d1cb6dfd42d0aba8e93b
XXmmRR <Xorex50@protonmial.com>
2025-09-28 23:11:02 +0300

    feat: init all apps

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local apps
+    'api',
+    'accounts',
+    'orders',
+    'products',
+
+    
 ]
 
 MIDDLEWARE = [
